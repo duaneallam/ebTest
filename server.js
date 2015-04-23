8de8e602b04b5495f74aa60b5a7e5154b2a75520
Duane Allam <duane.allam@gmail.com>
2015-04-23 14:38:55 +1000

    debug

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require("http");
 
+console.log(process.env.PORT);
+
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello Bitch!");
