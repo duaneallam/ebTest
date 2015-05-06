31f514f841ad217fedfa23629fbad81942ca42b8
Duane Allam <duane.allam@gmail.com>
2015-05-06 11:23:13 +1000

    spit req onto index page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
+var util = require('util');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log(req);
-  res.render('index', { title: 'Express', req: req });
+  res.render('index', { title: 'Express', req: util.inspect(req,{depth:4}) });
 });
 
 module.exports = router;
