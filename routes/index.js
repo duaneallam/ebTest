099467ed36caeeb7b5e4716f40ca8371a40de947
Duane Allam <duane.allam@gmail.com>
2015-05-06 11:34:56 +1000

    efwee

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', req: req.headers['x-forwarded-for'] });
+  res.render('index', { title: 'Express', req: req.headers['x-forwarded-for'].split(" ")[0] });
 });
 
 module.exports = router;
