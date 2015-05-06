ea8c315332527c39d4d623523d26573c062b098f
Duane Allam <duane.allam@gmail.com>
2015-05-06 11:31:27 +1000

    no message

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', req: util.inspect(req,{depth:4}) });
+  res.render('index', { title: 'Express', req: req.headers['x-forwarded-for'] });
 });
 
 module.exports = router;
